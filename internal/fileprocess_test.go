df3f55de64524436f7c91f5bce1f1d41db9fca99
Chen Lixiang <lixiang3608@outlook.com>
2020-04-15 00:02:22 +0800

    fix test

--- internal/fileprocess_test.go
+++ internal/fileprocess_test.go
@@ -1,34 +1,25 @@
 package internal
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/chenlx0/topN/config"
+	"github.com/chenlx0/topN/utils"
 )
-
-func TestGenMiddleFiles(t *testing.T) {
-	conf, err := config.InitConfig("../conf.json")
-	if err != nil {
-		t.Errorf("parse config failed: %v", err)
-	}
-	err = GenMiddleFiles(conf)
-	if err != nil {
-		t.Errorf("GenMiddleFiles failed: %v", err)
-	}
-}
 
 func TestInternal(t *testing.T) {
 	conf, err := config.InitConfig("../conf.json")
 	if err != nil {
 		t.Errorf("parse config failed: %v", err)
 	}
+	utils.GenSourceFile(conf.SourceFile, 128*1024*1024)
+	err = GenMiddleFiles(conf)
+	if err != nil {
+		t.Errorf("GenMiddleFiles failed: %v", err)
+	}
 	msgList, _ := Aggregate(conf)
 	err = GenMsgData(conf.SourceFile, msgList)
 	if err != nil {
 		t.Errorf("gen msg failed: %v", err)
-	}
-	for _, v := range msgList {
-		fmt.Printf("string: %s , occurs: %d\n", string(v.data), v.occurs)
 	}
 }
